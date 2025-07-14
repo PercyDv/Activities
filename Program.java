9608f059c81c62e7683f38cf0ada7e77eaf483ce
PercyDv <daviamaral2912@gmail.com>
2025-07-14 20:38:29 +0000

    correção de erros nas linhas 13 e 14

--- Program.java
+++ Program.java
@@ -3,15 +3,16 @@ package aplication;
 import java.util.Scanner;
 import entities.Rectangle;
 public class Program {
+    
     public static void main (String [] args) {
         Scanner kb = new Scanner (System.in);
         
-        Rectangle rectangle = new rectangle();
+        Rectangle rect = new rectangle();
 
         System.out.println("Enter Rectangle width and heigth: ");
-        rectangle.w = kb.nexDouble();
-        rectangle.h = kb.nexDouble();
+        rect.w = kb.nextDouble();
+        rect.h = kb.nextDouble();
 
-        System.out.printf("AREA = %.2f", rectangle.area());
+        System.out.printf("AREA = %.2f", rect.area());
     }
 }
